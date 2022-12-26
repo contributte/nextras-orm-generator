15707f7d79f3cab2addadc94c2393266aa6c852f
Milan Felix Šulc <rkfelix@gmail.com>
2022-12-26 19:52:50 +0100

    Tests: modernize

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,26 +1,13 @@
 <?php declare(strict_types = 1);
 
-/**
- * Test bootstrap.
- */
+use Ninjify\Nunjuck\Environment;
+
 if (@!include __DIR__ . '/../vendor/autoload.php') {
 	echo 'Install Nette Tester using `composer update --dev`';
 	exit(1);
 }
 
 // Configure environment
-Tester\Environment::setup();
-date_default_timezone_set('Europe/Prague');
-
-// Create temporary directory
-define('TEMP_DIR', __DIR__ . '/tmp/' . getmypid());
-@mkdir(dirname(TEMP_DIR)); // @ - directory may already exist
-Tester\Helpers::purge(TEMP_DIR);
-
-/**
- * Test functions
- */
-function test(Closure $function): void
-{
-	$function();
-}
+Environment::setupTester();
+Environment::setupTimezone();
+Environment::setupVariables(__DIR__);
